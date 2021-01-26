f72d57b9b79a96d23274619b2aea62e0afbd6d12
chris-vest <intellivision@pm.me>
2021-01-26 11:29:38 +0100

    Update status-left

--- scripts/dracula.sh
+++ scripts/dracula.sh
@@ -3,20 +3,20 @@
 export LC_ALL=en_US.UTF-8
 
 get_tmux_option() {
-  local option=$1
-  local default_value=$2
-  local option_value=$(tmux show-option -gqv "$option")
-  if [ -z $option_value ]; then
-    echo $default_value
-  else
-    echo $option_value
-  fi
+	local option=$1
+	local default_value=$2
+	local option_value=$(tmux show-option -gqv "$option")
+	if [ -z $option_value ]; then
+		echo $default_value
+	else
+		echo $option_value
+	fi
 }
 
 main()
 {
-  # set current directory variable
-  current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	# set current directory variable
+	current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
   # set configuration option variables
   show_flags=$(get_tmux_option "@dracula-show-flags" false)
@@ -28,7 +28,10 @@ main()
   show_refresh=$(get_tmux_option "@dracula-refresh-rate" 5)
 
   # Dracula Color Pallette
+  bglight='#343746'
+  bg='#282A36'
   white='#f8f8f2'
+  white_shade_4='#949491'
   gray='#44475a'
   dark_gray='#282a36'
   light_purple='#bd93f9'
@@ -43,30 +46,30 @@ main()
 
   # Handle left icon configuration
   case $show_left_icon in
-      smiley)
-          left_icon="☺";;
-      session)
-          left_icon="#S";;
-      window)
-          left_icon="#W";;
-      *)
-          left_icon=$show_left_icon;;
+	  smiley)
+		  left_icon="☺";;
+	  session)
+		  left_icon="#S";;
+	  window)
+		  left_icon="#W";;
+	  *)
+		  left_icon=$show_left_icon;;
   esac
 
   # Handle left icon padding
   padding=""
   if [ "$show_left_icon_padding" -gt "0" ]; then
-  padding="$(seq -f " " -s '' $show_left_icon_padding)"
+	  padding="$(seq -f " " -s '' $show_left_icon_padding)"
   fi
   left_icon="$left_icon$padding"
 
   case $show_flags in
-    false)
-      flags=""
-      current_flags="";;
-    true)
-      flags="#{?window_flags,#[fg=${dark_purple}]#{window_flags},}"
-      current_flags="#{?window_flags,#[fg=${light_purple}]#{window_flags},}"
+	  false)
+		  flags=""
+		  current_flags="";;
+	  true)
+		  flags="#{?window_flags,#[fg=${dark_purple}]#{window_flags},}"
+		  current_flags="#{?window_flags,#[fg=${light_purple}]#{window_flags},}"
   esac
 
   # sets refresh interval to every 5 seconds
@@ -78,11 +81,11 @@ main()
 
   # pane border styling
   if $show_border_contrast; then
-    tmux set-option -g pane-active-border-style "fg=${light_purple}"
-	tmux set-option -g pane-border-style "fg=${light_purple}"
+	  tmux set-option -g pane-active-border-style "fg=${light_purple}"
+	  tmux set-option -g pane-border-style "fg=${light_purple}"
   else
-    tmux set-option -g pane-active-border-style "fg=${dark_purple}"
-	tmux set-option -g pane-border-style "fg=${dark_purple}"
+	  tmux set-option -g pane-active-border-style "fg=${dark_purple}"
+	  tmux set-option -g pane-border-style "fg=${dark_purple}"
   fi
 
   # message styling
@@ -91,7 +94,7 @@ main()
   # status bar
   tmux set-option -g status-style "bg=${gray},fg=${white}"
 
-  tmux set-option -g status-left "#[bg=${green},fg=${dark_gray}]#{#[bg=${yellow}],} ${left_icon}"
+  tmux set-option -g status-left "#[bg=${green},fg=${dark_gray}]#{#[bg=${yellow}],} ${left_icon} "
 
   tmux set-window-option -g window-status-current-style "#[fg=${white},bg=${dark_purple}]"
   tmux set-window-option -g window-status-current-format "→ #I:#W ←"
@@ -100,6 +103,13 @@ main()
   tmux set-window-option -g window-status-format " #I:#W "
   tmux set-window-option -g window-status-activity-style "bold"
   tmux set-window-option -g window-status-bell-style "bold"
+
+  #set inactive/active window styles
+  tmux set-option -g window-style fg=colour247,bg=colour236
+  tmux set-option -g window-active-style fg=colour250,bg=colour237
+  tmux set-option -g window-style "fg=$white_shade_4,bg=$bg"
+  tmux set-option -g window-active-style "fg=$white,bg=$bglight"
+
 }
 
 # run main function
